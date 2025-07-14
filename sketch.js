e3e647e059c9e87fb5d2e3207ae36779aa162d5e
ajrhind <100628436+ajrhind@users.noreply.github.com>
2025-07-14 15:09:47 +1200

    dswd

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(600, 600); 
+  background(210);
+
+}
+function draw() {
+
+  //PENGUIN---------------------------------------------------------------
+
+  //body
+  fill(210);
+  ellipse(300,300,100,150);
+  fill(30);
+  ellipse(300,300,150,200);
+
+
+}
